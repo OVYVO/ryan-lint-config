df45fba7a90f22c8333fbdcea638b6f1dec215e6
ryanYan <vue12306@163.com>
2024-04-21 20:04:02 +0800

    :zap: perf: perf

--- packages/eslint/bin/index.js
+++ packages/eslint/bin/index.js
@@ -1,18 +1,19 @@
-import process from "node:process";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import pkgJson from "../package.json";
+// import process from "node:process";
+// import yargs from "yargs";
+// import { hideBin } from "yargs/helpers";
+// import pkgJson from "../package.json";
 
-yargs(hideBin(process.argv))
-  .scriptName("@ovyvo/eslint-config")
-  .command("*", "Run the initialization", (args) =>
-    args.option("yes", {
-      alias: "y",
-      description: "Skip prompts and use default values",
-      type: "boolean",
-    })
-  )
-  .showHelpOnFail(false)
-  .alias("h", "help")
-  .version("version", pkgJson.version)
-  .alias("v", "version");
+// yargs(hideBin(process.argv))
+//   .scriptName("@ovyvo/eslint-config")
+//   .command("*", "Run the initialization", (args) =>
+//     args.option("yes", {
+//       alias: "y",
+//       description: "Skip prompts and use default values",
+//       type: "boolean",
+//     })
+//   )
+//   .showHelpOnFail(false)
+//   .alias("h", "help")
+//   .version("version", pkgJson.version)
+//   .alias("v", "version");
+console.log(123);
