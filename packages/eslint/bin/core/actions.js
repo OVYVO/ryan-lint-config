73e045aaaae2fb9f9aa94fbd4c61902aa39b8772
ryanYan <vue12306@163.com>
2024-04-25 11:35:56 +0800

    :zap: perf: 优化

--- packages/eslint/bin/core/actions.js
+++ packages/eslint/bin/core/actions.js
@@ -2,16 +2,26 @@ import { cPrimary } from "utils";
 import inquirer from "inquirer";
 
 export const handlerInit = async () => {
-  const { needInstallDependencies } = await inquirer.prompt({
-    type: "confirm",
-    name: "needInstallDependencies",
-    message: `Do you need to install base dependencies for you`,
+  // const { needInstallDependencies } = await inquirer.prompt({
+  //   type: "confirm",
+  //   name: "needInstallDependencies",
+  //   message: `Do you need to install base dependencies for you`,
+  // });
+  const { langType } = await inquirer.prompt({
+    type: "list",
+    name: "langType",
+    message: `Which language is used in your project`,
+    choices: [
+      { name: "JavaScript", value: 1 },
+      { name: "TypeScript", value: 2 },
+    ],
   });
   const { needCreateignoreFile } = await inquirer.prompt({
     type: "confirm",
     name: "needCreateignoreFile",
     message: `Do you need to create the .eslintignore file for you`,
   });
-  cPrimary(`安装依赖:${needInstallDependencies}`);
+  // cPrimary(`安装依赖:${needInstallDependencies}`);
   cPrimary(`生成ignoreFile:${needCreateignoreFile}`);
+  cPrimary(`使用哪种语言:${langType}`);
 };
--- packages/eslint/bin/core/create.js
+++ packages/eslint/bin/core/create.js
@@ -3,6 +3,6 @@ import { handlerInit } from "./actions.js";
 export const createCommands = (program) => {
   program
     .command("init")
-    .description("Initialize eslint configuration.")
+    .description("Initialize eslint configuration. Currently only supports Vue")
     .action(handlerInit);
 };
