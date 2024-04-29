8339223142faabce1129290c3664220574353f50
ryanYan <vue12306@163.com>
2024-04-29 17:13:34 +0800

    :zap: perf: utils添加新函数

--- packages/eslint/bin/handler/handlerInit.js
+++ packages/eslint/bin/handler/handlerInit.js
@@ -3,32 +3,32 @@ import * as t from "../template/index.js";
 
 export const initConfig = async ({ langType, needCreateignoreFile } = {}) => {
   try {
-    u.loading.start({ text: "正在为您安装基础依赖..." });
+    u.loading.start({ text: "👷正在为您安装基础依赖..." });
     await u.commandSpawn("pnpm", ["add", "@ovyvo/eslint-config", "-D"], {
       cwd: process.cwd(),
       stdio: "pipe",
     });
-    u.loading.succeed("基础依赖安装完毕");
+    u.loading.succeed(u.wSuccess("🚀基础依赖安装完毕"));
     if (langType == 2) {
-      u.loading.start({ text: "正在为您安装附加依赖..." });
+      u.loading.start({ text: "👷正在为您安装附加依赖..." });
       await u.commandSpawn("pnpm", ["add", "@typescript-eslint/parser", "-D"], {
         cwd: process.cwd(),
         stdio: "pipe",
       });
-      u.loading.succeed("附加依赖安装完毕");
+      u.loading.succeed(u.wSuccess("🚀附加依赖安装完毕"));
     }
-    u.loading.start({ text: "为您写入.eslint.cjs配置文件..." });
+    u.loading.start({ text: "👷为您写入.eslint.cjs配置文件..." });
     const eslintFileContent = t.eslintConfig(langType);
     await u.writeFile(process.cwd(), ".eslint.cjs", eslintFileContent);
-    u.loading.succeed(".eslint.cjs配置文件写入完毕...");
+    u.loading.succeed(u.wSuccess("🚀.eslint.cjs配置文件写入完毕..."));
     if (needCreateignoreFile) {
-      u.loading.start({ text: "为您写入.eslintignore配置文件..." });
+      u.loading.start({ text: "👷为您写入.eslintignore配置文件..." });
       const ignoreFileContent = t.eslintIgnore();
       await u.writeFile(process.cwd(), ".eslintignore", ignoreFileContent);
-      u.loading.succeed(".eslintignore配置文件写入完毕...");
+      u.loading.succeed(u.wSuccess("🚀.eslintignore配置文件写入完毕..."));
     }
   } catch (error) {
-    u.cError(`抱歉, 初始化失败！报错信息: ${error}`);
+    u.cError(`🚨抱歉, 初始化失败！报错信息: ${error}`);
     u.loading.clear();
   }
 };
--- packages/utils/modules/chalk.js
+++ packages/utils/modules/chalk.js
@@ -4,8 +4,13 @@
  */
 
 import chalk from "chalk";
-
+//输出
 export const cPrimary = (text) => console.log(chalk.hex("#1890ff").bold(text));
 export const cError = (text) => console.log(chalk.hex("#f5222d").bold(text));
 export const cWarning = (text) => console.log(chalk.hex("#faad14").bold(text));
 export const cSuccess = (text) => console.log(chalk.hex("#52c41a").bold(text));
+//着色
+export const wPrimary = (text) => chalk.hex("#1890ff").bold(text);
+export const wError = (text) => chalk.hex("#f5222d").bold(text);
+export const wWarning = (text) => chalk.hex("#faad14").bold(text);
+export const wSuccess = (text) => chalk.hex("#52c41a").bold(text);
