d42751b4f35a7b46fd3bdf7e92f93faccbe73afe
ryanYan <vue12306@163.com>
2024-05-06 10:17:37 +0800

    :zap: perf: perf

--- packages/eslint/bin/handler/handlerInit.js
+++ packages/eslint/bin/handler/handlerInit.js
@@ -8,24 +8,24 @@ export const initConfig = async ({ langType, needCreateignoreFile } = {}) => {
       cwd: process.cwd(),
       stdio: "pipe",
     });
-    u.loading.succeed(u.wSuccess("🚀基础依赖安装完毕"));
+    u.loading.succeed(u.wSuccess("🚀 基础依赖安装完毕"));
     if (langType == 2) {
       u.loading.start({ text: "👷正在为您安装附加依赖..." });
       await u.commandSpawn("pnpm", ["add", "@typescript-eslint/parser", "-D"], {
         cwd: process.cwd(),
         stdio: "pipe",
       });
-      u.loading.succeed(u.wSuccess("🚀附加依赖安装完毕"));
+      u.loading.succeed(u.wSuccess("🚀 附加依赖安装完毕"));
     }
     u.loading.start({ text: "👷为您写入.eslint.cjs配置文件..." });
     const eslintFileContent = t.eslintConfig(langType);
     await u.writeFile(process.cwd(), ".eslint.cjs", eslintFileContent);
-    u.loading.succeed(u.wSuccess("🚀.eslint.cjs配置文件写入完毕..."));
+    u.loading.succeed(u.wSuccess("🚀 .eslint.cjs配置文件写入完毕..."));
     if (needCreateignoreFile) {
       u.loading.start({ text: "👷为您写入.eslintignore配置文件..." });
       const ignoreFileContent = t.eslintIgnore();
       await u.writeFile(process.cwd(), ".eslintignore", ignoreFileContent);
-      u.loading.succeed(u.wSuccess("🚀.eslintignore配置文件写入完毕..."));
+      u.loading.succeed(u.wSuccess("🚀 .eslintignore配置文件写入完毕..."));
     }
   } catch (error) {
     u.cError(`🚨抱歉, 初始化失败！报错信息: ${error}`);
