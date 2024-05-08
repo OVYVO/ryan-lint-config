a42f54ab8a748e1edee7bf7c066070a0d57360c8
ryanYan <vue12306@163.com>
2024-05-08 18:19:49 +0800

    :sparkles: feat: utilsRollup打包

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,28 +1,23 @@
 import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import nodePolyfills from "rollup-plugin-polyfill-node";
-import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "lib/index.js",
+  input: "modules/index.ts",
   output: {
-    file: "bin/index.js",
+    name: "cliUtils",
+    file: "index.js",
     format: "umd",
     globals: {},
   },
   plugins: [
-    json(),
-    resolve({
-      preferBuiltins: true,
-      exclude: "node_modules/**",
-    }),
-    commonjs({
-      exclude: "node_modules/**",
-    }),
-    nodePolyfills(),
     babel({
       exclude: "node_modules/**",
     }),
+    typescript({
+      compilerOptions: { lib: ["es5", "es6", "dom"], target: "es5" },
+      exclude: "node_modules/**",
+    }),
+    nodePolyfills(),
   ],
 };
