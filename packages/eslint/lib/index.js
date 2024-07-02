baa8f1c86ebf1df41fe9559556bd9b0f4d5de859
ryanYan <vue12306@163.com>
2024-07-02 10:46:39 +0800

    :bug: fix: 代码注释

--- packages/eslint/lib/index.js
+++ packages/eslint/lib/index.js
@@ -1,12 +1,12 @@
 #! /usr/bin/env node
 import { Command } from "commander";
-// import { helpOptions } from "./core/help.js";
+import { helpOptions } from "./core/help.js";
 import { createCommands } from "./core/create.js";
 
 const program = new Command();
 
 // 创建辅助指令
-// helpOptions(program);
+helpOptions(program);
 // 添加其他指令
 createCommands(program);
 
