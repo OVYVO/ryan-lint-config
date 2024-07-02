9a05e4a6866424e1815ed34e7396fa4d4145db95
ryanYan <vue12306@163.com>
2024-07-02 10:25:49 +0800

    :bug: fix: 修复打包报错fs.realPath.native问题

--- packages/eslint/lib/core/help.js
+++ packages/eslint/lib/core/help.js
@@ -1,7 +1,7 @@
-import { pkg } from "@ovyvo/cli-utils";
-export async function helpOptions(program) {
-  const pkgFileContent = await pkg.resolvePkg(process.cwd());
-  program.name(pkgFileContent.name);
-  program.version(pkgFileContent.version);
-  program.usage(`<command> [option]`);
-}
+// import { pkg } from "@ovyvo/cli-utils";
+// export async function helpOptions(program) {
+//   const pkgFileContent = await pkg.resolvePkg(process.cwd());
+//   program.name(pkgFileContent.name);
+//   program.version(pkgFileContent.version);
+//   program.usage(`<command> [option]`);
+// }
--- packages/eslint/rollup.config.js
+++ packages/eslint/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import json from "@rollup/plugin-json";
 
 export default {
@@ -12,7 +11,6 @@ export default {
   },
   plugins: [
     json(),
-    nodePolyfills(),
     resolve({
       preferBuiltins: true,
       exclude: "node_modules/**",
