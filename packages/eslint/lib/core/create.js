9ec549af6784fc88ae19046bbc5ff8bb1e59351e
ryanYan <vue12306@163.com>
2024-07-22 14:31:40 +0800

    :recycle: refactor: 放弃eslint配置命令行交互模式

--- packages/eslint/lib/core/create.js
+++
@@ -1,8 +0,0 @@
-import { handlerInit } from "./actions.js";
-
-export const createCommands = (program) => {
-  program
-    .command("init")
-    .description("Initialize eslint configuration. Currently only supports Vue")
-    .action(handlerInit);
-};
--- packages/eslint/lib/template/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./createEslintconfig.js";
-export * from "./createEslintignore.js";
