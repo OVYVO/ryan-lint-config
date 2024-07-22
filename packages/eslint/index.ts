95f824f961d76acc158c0a7bdaf0dc8f2523000b
ryanYan <vue12306@163.com>
2024-07-22 14:50:06 +0800

    :building_construction: fram: eslint-config基本架构搭建

---
+++ packages/eslint/index.ts
@@ -0,0 +1,4 @@
+import type { Options } from "./types";
+import { createVueConfig, createJsConfig, createTsConfig } from "./configs";
+
+export const defineConfig = (config: Partial<Options>) => {};
