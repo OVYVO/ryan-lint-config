1baeef76858e5ea9005f5b586a8842463faa7e4b
ryanYan <vue12306@163.com>
2024-07-24 14:45:26 +0800

    :zap: perf: 打包测试

--- packages/eslint/rollup.config.js
+++ packages/eslint/rollup.config.js
@@ -2,7 +2,7 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-// import terser from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "index.ts",
@@ -12,9 +12,9 @@ export default {
   },
   plugins: [
     json(),
-    resolve({ exclude: "node_modules/**", exportConditions: ["node"] }),
+    resolve({ exclude: "node_modules/**" }),
     commonjs({ exclude: "node_modules/**" }),
     typescript({ exclude: "node_modules/**" }),
-    // terser(),
+    terser(),
   ],
 };
