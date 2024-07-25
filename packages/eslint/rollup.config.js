dfef934d7792357b9181601d5e8e12a9144dca51
ryanYan <vue12306@163.com>
2024-07-25 15:24:52 +0800

    :wrench: config: 打包压缩

--- packages/eslint/rollup.config.js
+++ packages/eslint/rollup.config.js
@@ -2,7 +2,7 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-// import terser from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "index.ts",
@@ -22,6 +22,6 @@ export default {
     resolve({ exclude: "node_modules/**" }),
     commonjs({ exclude: "node_modules/**" }),
     typescript({ exclude: "node_modules/**" }),
-    // terser(),
+    terser(),
   ],
 };
